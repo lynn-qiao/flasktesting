86e4f64712e29bb37cea44f5b625b621ead2c11f
lynn-qiao <63971654+lynn-qiao@users.noreply.github.com>
2020-06-27 12:39:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+import tickgraph
+from bokeh.embed import components
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-  return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
+    if request.method == 'POST':
+        tkr=request.form['ticker']
+        script, div = tickgraph.get_a_graph(tkr)
+        # script, div = components(p)
+        # return render_template('stock.html')
+        return render_template('out.html', script=script, div=div)
+    else:
+        return render_template('frontpg.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+     app.debug=True
+     app.run(port=33507)
+
+
+
+ 
+
+
+
---
+++ tickgraph.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 22:24:43 2020
+
+@author: xqiao
+"""
+
+import requests
+import pandas as pd
+
+from bokeh.plotting import figure
+# from bokeh.embed import components
+from bokeh.resources import CDN
+from bokeh.embed import file_html, components
+from bokeh.io import save
+
+# #receive sticker and get data
+def get_df_by_sticker(inp):
+    sticker= str(inp)
+    #set empty df
+    df=pd.DataFrame()
+
+    #get data with symbol=inp
+    bits1="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+    bits2="&apikey=96AQXV2FPIVZ1TOZ"
+    # samp="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=96AQXV2FPIVZ1TOZ"
+    r=requests.get (bits1+sticker+bits2)
+    
+    data=r.json()
+    data2=pd.json_normalize(data['Time Series (Daily)'],max_level=0)
+    colnm=data2.columns
+    for date in colnm: #get dataframe
+        out= data2[date]
+        out1=pd.json_normalize(out) #5 columns
+        out1['time']=date #6 column
+        df=df.append(out1, ignore_index=True)
+    df=pd.DataFrame(df)
+    return df
+
+#graph it
+def get_a_graph(inp):
+    df_s=get_df_by_sticker(inp)
+    df_s['time'] = pd.to_datetime(df_s['time'])
+    tmin=df_s['time'].min()
+    tmax=df_s['time'].max()
+    y=pd.to_numeric(pd.Series(df_s['4. close']))
+    p1 = figure(x_axis_type="datetime", x_range = (tmin, tmax), y_range=(min(y),max(y)),title="Recent Stock Closing Prices")
+    p1.grid.grid_line_alpha=0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Close Price'
+    p1.line(df_s['time'], df_s['4. close'], color='#A6CEE3', legend_label=inp)
+    # save(p1, filename='templates/stock.html',resources=CDN,title='my plot')
+    script, div = components(p1)
+    return script, div
